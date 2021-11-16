700049fb713775937ab777abe88c399d0e6494ef
JakubKocerha <jakubbubble@gmail.com>
2021-11-16 19:49:04 +0000

    Retrieve latest stock numbers

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
-
+from pprint import pprint
 
 
 SCOPE = [
@@ -17,10 +17,8 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 def get_sales_data():
     """
     Get sales figures input from the user
-    
     While loop above and if statement below + setting return False/True allow us to run the input again in case invalid values without need of restarting the program
     This block of code will run unless the data are valid
-    
     Run a while loop to collect a valid string of data from the user via
     terminal, which must be a string of 6 numbers separated by commas. 
     The loop will repeatedly request data, until it is valid
@@ -32,9 +30,11 @@ def get_sales_data():
 
         data_str = input("Enter your data here: ")
     
-
-        sales_data = data_str.split(",") # The split() method returns the broken up values as a list.
+        """
+        The split() method returns the broken up values as a list.
         # print(sales_data) Prints sales data separated by a comma with split() into terminal.
+        """
+        sales_data = data_str.split(",")
         
         """
         While loop above and if statement below + setting return False/True allow us to run the input again in case invalid values without need of restarting the program
@@ -72,6 +72,27 @@ def update_sales_worksheet(data):
     sales_worksheet = SHEET.worksheet("sales") # the value "sales" relates to the name of the worksheet in our google spreadsheet
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type.
+
+    The surplus is defined as the sales figure subtracted from the stock:
+    - Positive surplis indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
+    """
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+print("Welcome to Love Sandwiches Data Automation")
+main()
